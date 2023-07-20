943f47dd111f946fc9acb2fcab71a2e8ee46417a
Szajerro <michal.pawel.szajer@gmail.com>
2023-07-20 19:40:34 +0200

    updated script and added requirements

--- poziom_wisly.py
+++ poziom_wisly.py
@@ -1,5 +1,4 @@
 # Data manipulation:
-import numpy as np
 import pandas as pd
 import datetime as dt
 
@@ -12,26 +11,27 @@ import gspread
 import json
 from os import environ
 
+scopes = ['https://www.googleapis.com/auth/spreadsheets',
+          'https://www.googleapis.com/auth/drive']
+credentials_google = service_account.Credentials.from_service_account_file(
+    environ['PERSONAL_GCLOUD_KEY'],scopes=scopes)
+gspread_client = gspread.Client(auth=credentials_google)
+spreadsheet_id = '1eJSD3_ma89LpI38C4e_xYDUixB6cJ7d6IkVgM0kxVxQ'
+
+worksheet_name = 'Parametry pomostu'
+spreadsheet = gspread_client.open_by_key(spreadsheet_id)
+worksheet = spreadsheet.worksheet(worksheet_name)
+pomost_df = pd.DataFrame(worksheet.get_all_values()[1:],columns=worksheet.get_all_values()[0])
+pomost_df.set_index('Key',inplace=True)
+pomost_dict = dict(pomost_df['Value'])
 
 hydro = requests.get("http://danepubliczne.imgw.pl/api/data/hydro/").text
 hydro_data = json.loads(hydro)
 for item in hydro_data:
     if item['id_stacji'] == '152210170':
-        bulwary_dict = item
+        bulwary_dict = item | pomost_dict
 bulwary_df = pd.DataFrame(pd.Series(bulwary_dict)).T
-scopes = ['https://www.googleapis.com/auth/spreadsheets',
-          'https://www.googleapis.com/auth/drive']
-credentials_google = service_account.Credentials.from_service_account_file(
-    environ['PERSONAL_GCLOUD_KEY'],scopes=scopes)
-project_id = 'rtbhouse-business-apac'
-gspread_client = gspread.Client(auth=credentials_google)
-spreadsheet_id = '1eJSD3_ma89LpI38C4e_xYDUixB6cJ7d6IkVgM0kxVxQ'
-worksheet_name = 'Poziom wody'
-
 bulwary_df.fillna('',inplace=True)
-spreadsheet = gspread_client.open_by_key(spreadsheet_id)
-worksheet = spreadsheet.worksheet(worksheet_name)
-spreadsheet_data = pd.DataFrame(worksheet.get_all_values()[1:],columns=worksheet.get_all_values()[0])
 
 def to_spreadsheet_date(x):
     time = dt.datetime.strptime(x,'%Y-%m-%d %H:%M:%S')
@@ -42,10 +42,36 @@ def to_spreadsheet_date(x):
     return delta
 
 
-bulwary_df['stan_wody_data_pomiaru'] = bulwary_df['stan_wody_data_pomiaru'].apply(lambda x: to_spreadsheet_date(x))
+worksheet_name = 'Poziom wody'
+spreadsheet = gspread_client.open_by_key(spreadsheet_id)
+worksheet = spreadsheet.worksheet(worksheet_name)
+spreadsheet_data = pd.DataFrame(worksheet.get_all_values()[1:],columns=worksheet.get_all_values()[0])
+
 final_df = pd.concat([bulwary_df,spreadsheet_data],axis=0)
+final_df.fillna('',inplace=True)
+
+final_df['stan_wody_data_pomiaru'] = final_df['stan_wody_data_pomiaru'].apply(lambda x: to_spreadsheet_date(x) if x != '' else '')
+final_df['zjawisko_zarastania_data_pomiaru'] = final_df['zjawisko_zarastania_data_pomiaru'].apply(lambda x: to_spreadsheet_date(x) if x != '' else '')
+final_df['stan_wody'] = final_df['stan_wody'].apply(lambda x: int(x) if x != '' else '')
+final_df['zjawisko_zarastania'] = final_df['zjawisko_zarastania'].apply(lambda x: int(x) if x != '' else '')
+
+final_df['temperatura_wody_data_pomiaru'] = final_df['temperatura_wody_data_pomiaru'].apply(lambda x: to_spreadsheet_date(x) if x != '' else '')
+final_df['zjawisko_lodowe_data_pomiaru'] = final_df['zjawisko_lodowe_data_pomiaru'].apply(lambda x: to_spreadsheet_date(x) if x != '' else '')
+final_df['temperatura_wody'] = final_df['temperatura_wody'].apply(lambda x: int(x) if x != '' else '')
+final_df['zjawisko_lodowe'] = final_df['zjawisko_lodowe'].apply(lambda x: int(x) if x != '' else '')
+
+
 dataframe_to_update = final_df
 worksheet.update([dataframe_to_update.columns.values.tolist()] + dataframe_to_update.values.tolist())
 
+formatting = {
+    'numberFormat': {
+        "type":"DATE",
+        "pattern": "yyyy-mm-dd HH:MM:SS"
+    }
+}
 
-
+worksheet.format('F2:F995',formatting)
+worksheet.format('H2:H995',formatting)
+worksheet.format('J2:J995',formatting)
+worksheet.format('L2:L995',formatting)
